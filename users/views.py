8abc905a9f08daacadbe0575f3c1c785ee2e0778
maa7220 <123408096+maa7220@users.noreply.github.com>
2023-03-05 11:28:09 +0200

    Update views.py

--- users/views.py
+++ users/views.py
@@ -312,7 +312,10 @@ class SignupPatients(generics.GenericAPIView):
 
             return Response(data=response, status=status.HTTP_201_CREATED)
         else:
-            return Response({serializer.errors})
+            new_error = {}
+            for field_name, field_errors in serializer.errors.items():
+                new_error[field_name] = field_errors[0]
+            return Response(new_error, status=status.HTTP_400_BAD_REQUEST)
 
 
 # ----- Patients for admin ---------
