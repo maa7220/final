ab9c0389b7c13e9f2964cab59f3705ad7e2b964b
maa7220 <123408096+maa7220@users.noreply.github.com>
2023-03-27 16:42:30 +0200

    Update 0007_user_address_user_status.py

--- users/migrations/0007_user_address_user_status.py
+++ users/migrations/0007_user_address_user_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0007_user_address_user_status.py'),
+        ('users', '0007_user_address_user_status'),
     ]
 
     operations = [
